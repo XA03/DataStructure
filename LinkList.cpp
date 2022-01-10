cb3b2db0673643546e067acbf2f9c2a8d238ad4d
Chu <s033390431@gmail.com>
2022-01-10 20:27:29 +0800

    listnode declartion

---
+++ LinkList.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class node{
+  private:
+        int value;
+        node* next;
+  public:
+        node():value(0),next(NULL){};//constructor後的冒號是給 class data member assign的方法 如果是const data member更適合這種assignment(賦值、=)
+        node(int input):value(input),next(NULL){};
+};
+
+
+
+
+int main(){
+
+    
+
+
+
+    return 0;
+}
