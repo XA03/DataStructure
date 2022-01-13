1c168312844653e3e994cc97fcc276e9f3b36dfb
Chu <s033390431@gmail.com>
2022-01-13 16:25:09 +0800

    SinglyLinkList 基本完成

--- SinglyLinkList.cpp
+++ SinglyLinkList.cpp
@@ -12,34 +12,35 @@ using namespace std;
 //其中指標是可以沒指到東西的(NULL)，也就是說只宣告指標而不給予其節點是沒辦法利用節點中通往下一個節點的指標變數(*next)，所以會連不起來。
 //另外注意：MemberFunction中的操作可以直接影響DataMember，所以在使用上要記得用Temp來取代直接使用DataMebmer
 
-class SinglyLinkList;                                 //因為需要freind 所以得先宣告在這裡
+class SinglyLinkList; //因為需要freind 所以得先宣告在這裡
 
-class node{                                           // SinglyLinkList 中的單位節點
-      private:
-            int value;                                //節點本身的值
-            node* next;                               //指向下一個節點的指標
-      public:
-            node():value(0),next(NULL){};             //constructor後的冒號是給 class data member assign的方法 如果是const data member更適合這種assignment(賦值、=)
-            node(int input):value(input),next(NULL){};
-  
-      friend class SinglyLinkList;                    //表示class SinglyLinkList中可以access class node裡的 所有東西
+class node
+{ // SinglyLinkList 中的單位節點
+private:
+      int value;  //節點本身的值
+      node *next; //指向下一個節點的指標
+public:
+      node() : value(0), next(NULL){};
+      node(int input) : value(input), next(NULL){}; //constructor後的冒號是給 class data member assign的方法 如果是const data member更適合這種assignment(賦值、=)
+      friend class SinglyLinkList;                  //表示class SinglyLinkList中可以access class node裡的 所有東西
 };
 
-class SinglyLinkList{                                 // SinglyLinkList 單向鏈節串列 (單向鏈表)
-      private:
-            int length;                               //SinglyLinkList長度
-            node* head;                               //SinglyLinkList的起點
-      public:
-            SinglyLinkList():length(0),head(NULL){};
-            int Length();                             //給出串列長度
-            node* Position(int index);                //回傳從頭開始第index個的節點
-            void Shownode(int index);                 //印出從頭開始第index個的節點
-            void ShowList();                          //印出整個串列
-            void Front_insert(int index,int value);   //把值為value節點插在第index個節點的前面
-            void Back_insert(int index, int value);   //把值為value節點插在第index個節點的後面
-            void Delete_node(int value);              //刪除值為value的節點
-            void Clear();                             //清空整個串列
-            void Inverse();                           //將串列的方向調換，原本的起點變成尾巴
+class SinglyLinkList
+{ // SinglyLinkList 單向鏈節串列 (單向鏈表)
+private:
+      int length; //SinglyLinkList長度
+      node *head; //SinglyLinkList的起點
+public:
+      SinglyLinkList() : length(0), head(NULL){};
+      int Length();                            //給出串列長度
+      node *Position(int index);               //回傳從頭開始第index個的節點
+      void Shownode(int index);                //印出從頭開始第index個的節點
+      void ShowList();                         //印出整個串列
+      void Front_insert(int value, int index); //把值為value節點插在第index個節點的前面
+      void Back_insert(int value, int index);  //把值為value節點插在第index個節點的後面
+      void Delete_node(int index);             //刪除第index個節點
+      void Clear();                            //清空整個串列
+      void Inverse();                          //將串列的方向調換，原本的起點變成尾巴
 };
 
 int SinglyLinkList::Length(){
@@ -68,7 +69,7 @@ node* SinglyLinkList::Position(int index){
 //但是換成 A是一個空箱子 B是一個裝著水的箱子 你就可以說 A跟B一定是不一樣的箱子 因為A根本沒裝東西
 //NULL的意思就是指這個箱子(指標)裡面沒裝東西(物件)的意思
 
-//thi指標說明：待補
+//this指標說明：待補
 
 
 
@@ -80,7 +81,7 @@ void SinglyLinkList::Shownode(int index){
 
 void SinglyLinkList::ShowList(){ 
       
-      node* temp=head;//避免使用head造成串列位置不對
+      node* temp=head;                                //避免使用head造成串列位置不對
 
       if(!temp)cout<<"SinglyLinkList is empty!\n";
       else{
@@ -92,8 +93,8 @@ void SinglyLinkList::ShowList(){
       }
 }
 
-void SinglyLinkList::Front_insert(int index,int value){
-      node* newnode=new node(value);                  //宣告要插入的新節點
+void SinglyLinkList::Front_insert(int value,int index=1){
+      node* newnode=new node(value);                 
 
       if(index==1||!head){                                             
             newnode->next=head;
@@ -111,8 +112,8 @@ void SinglyLinkList::Front_insert(int index,int value){
       } 
 }
 
-void SinglyLinkList::Back_insert(int index,int value){
-      node* newnode=new node(value);                  //宣告要插入的新節點         
+void SinglyLinkList::Back_insert(int value,int index=1){
+      node* newnode=new node(value);                         
 
       if(!head){
           head=newnode;
@@ -135,12 +136,58 @@ void SinglyLinkList::Back_insert(int index,int value){
       }
 }
 
-void Delete_node(int value){
-      node* dnode;
+void SinglyLinkList::Delete_node(int index){
+      if(!head)cout<<"No any node can be deleted!\n";
+      else if(index<1||index>length)cout<<"You can't delete the node out of List range!\n";
+      else{
+            if(index==1){
+                  node* dnode=head;
+                  head=head->next;
+                  delete(dnode);
+                  length--;
+            }
+            else{
+                  node *previous=this->Position(index-1),*dnode=this->Position(index);
+                  previous->next=dnode->next;
+                  delete (dnode);
+                  length--;
+            }
+      }
+}
+
+void SinglyLinkList::Clear(){
+      while(head){
+            node* temp=head;
+            head=head->next;
+            delete(temp);
+      }
+}
+
+void SinglyLinkList::Inverse(){
+      
+      if(length==0||length==1)return ;
+      node *trail=NULL,*mid=NULL;
+
+      while(head){
+            trail=mid;                                
+            mid=head;                                 
+            head=head->next;                          
+            mid->next=trail;                         
+      }
+      head=mid;
 }
 
 int main(){
+
       SinglyLinkList test;
+
+      test.Back_insert(0);
+      for(int i=1;i<11;i++){
+            test.Back_insert(i,i);
+      }
+      test.ShowList();
+
+
 
 
       return 0;
