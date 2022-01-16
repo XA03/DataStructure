f48e252e0dabbef68d8088060ead424c0c9ac3ca
Chu <s033390431@gmail.com>
2022-01-17 03:43:51 +0800

    BinarySearch 基礎應用

--- Search Algorithm/BinarySearch.cpp
+++ Search Algorithm/BinarySearch.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+//參考文章：https://www.cnblogs.com/grandyang/p/6854825.html
+
 //BinarySearch 二元搜尋法，非常具代表性的一種搜尋法，這個搜尋法需要容器內的元素是已經排序好的。每次我們都從中間抓一個元素
 //比較它和我們的搜尋目標。大於代表要往後半部找，小於表示往前半部找，等於表示找到了。好處是範圍減少得很快
 
@@ -29,7 +31,6 @@ int BinarySearchNE(vector<int>arr,int left,int right,int target){
             return mid;
         }
     }
-
     return ret;
 }
 
@@ -45,7 +46,6 @@ int BinarySearchE(vector<int>arr,int left,int right,int target){
             return mid;
         }
     }
-
     return ret;
 }
 
@@ -53,19 +53,63 @@ int BinarySearchE(vector<int>arr,int left,int right,int target){
 //接下來討論Binary Search除了找指定元素外還可以做甚麼
 
 //1.搜尋第一個在容器中>=target的值
-
-int BSgeNum(vector<int>arr,int left,int right,int target){
-    
+//ge:greater and equal >=
+//因為是找出來位置，不需要等於的條件(就算等於我們也要找出第一個或是最後一個)
+int First_BSgeTargetNE(vector<int>arr,int left,int right,int target){
+    while(left<right){
+        int mid=left+(right-left)/2;
+        if(arr[mid]<target)left=mid+1;
+        else right=mid;//=target時縮right是因為要找第一個
+    }
+    return right;//這裡return right或left沒差別，因為你停止的條件是left==right
 }
 
+int First_BSgeTargetE(vector<int>arr,int left,int right,int target){
+    while(left<=right){
+        int mid=left+(right-left)/2;
+        if(arr[mid]<target)left=mid+1;
+        else right=mid-1;//right會一直縮小直到(arr[mid]<target)，又因為mid-1所以最後必然是left=right+1
+    }
+    return left;//或是right+1
+}
+//稍加變形可以變成找出第一個比目標元素大的位置也就是2.
+
+//2.搜尋第一個在容器中>target的值
+
+int First_BSgTargetNE(vector<int>arr,int left,int right,int target){
+    while(left<right){
+        int mid=left+(right-left)/2;
+        if(arr[mid]<=target)left=mid+1;//變成=target時不斷增加mid
+        else right=mid;
+    }
+    return left;
+}
+
+int First_BSgTargetE(vector<int>arr,int left,int right,int target){
+    while(left<=right){
+        int mid=left+(right-left)/2;
+        if(arr[mid]<=target)left=mid+1;
+        else right=mid-1;
+    }
+    return left;
+}
+//我們整理一下全部的搜尋關係 1.=target 2.第一個>=target 3.第一個>target (最後一個>=target位置+1) 4.第一個<=target 5.第一<target
+//6.最後一個<=target 7.最後一個<target 
+//整理以上關係可得出：4.沒意義，因為<=2.的答案的位置全部都是 (另一種問法是<target的元素有幾個) 不需要特別做一個新的Function 用2.就好
+//5.跟4一樣只是變成<2.而已  6.跟7.很像4跟5 只是從一個區間變成2.一個或是2.前一個位置而已
+//能夠問的問題不外乎就是 位置或是個數再不然就是值 基本上有位置都可以推出其他答案 
+//當然，二元搜尋法的用途還有很多種。個人理解是一種區間決定的演算法，而我們的問題答案是區間的 頂點、長度這樣
 
 
 int main(){
 
-    vector<int>test={1,2,3,4,5,6,7,8,9,10};
-    cout<<BinarySearchNE(test,0,10,10)<<endl;   //找不到 因為第一次結束後5已經變成邊界了且不包含5
-    cout<<BinarySearchE(test,0,9,10)<<endl;
-
-
+    vector<int>test={1,2,3,4,5,6,7,8,9,10},
+               ge={1,2,3,3,3,4,4,4,4,7};
+    //cout<<BinarySearchNE(test,0,10,10)<<endl;   //找不到 因為第一次結束後5已經變成邊界了且不包含5
+    //cout<<BinarySearchE(test,0,9,10)<<endl;
+    cout<<First_BSgeTargetNE(ge,0,ge.size(),3)<<endl;
+    cout<<First_BSgeTargetE(ge,0,ge.size()-1,3)<<endl;
+    cout<<First_BSgTargetNE(ge,0,ge.size(),3)<<endl;
+    cout<<First_BSgTargetE(ge,0,ge.size()-1,3)<<endl;
     return 0;
 }
