d98779d731ea81b822170cd287d034e0afd62494
Chu <s033390431@gmail.com>
2022-01-22 20:21:47 +0800

    Partition and Testing bug

--- Sorting Algorithm/ComparingSort.cpp
+++ Sorting Algorithm/ComparingSort.cpp
@@ -34,6 +34,19 @@ void showlist(vector<int>arr){
     cout<<endl;
 }
 
+void partition(vector<int>arr,int pivot){
+    int i=-1;
+    for(int iter=0;iter<arr.size();iter++){
+        if(iter==pivot)continue;
+        if(arr[iter]<arr[pivot]){
+            i++;
+            if(i==pivot)pivot=iter;
+            swap(arr[i],arr[iter]);
+        }
+    }
+    swap(arr[i+1],arr[pivot]);
+}
+
 void BubbleSort(vector<int>arr){
 
     //流程說明：首先我們將容器分為未排序和已排序的部分，如果前一筆>後一筆則交換，只要還有未排序的部分，就繼續執行下去直到容器尾巴
@@ -96,12 +109,32 @@ void InsertionSort(vector<int>arr){
     }
 }
 
+void QuickSort(vector<int>arr,int left,int right){
+
+    //流程說明選一個基準值(Pivot)，把比Pivot小的擺左邊，比Pivot大的擺右邊 (Partition)
+    //分別對Pivot左邊序列和Pivot右邊序列重複動作(Recursive)，直到序列長度=1或沒有元素
+    //Divide and Conquer的典型，將原陣列的排序問題分成無數個長度為一的序列排序問題
+    //Pivot可以是隨機、第一個、中位數
+    //Best Case:O(nlogn) 第一個元素剛好是中位數
+    //Average Case:O(nlogn)
+    //Worst Case:O(n^2) 由小到大或由大到小
+
+    //SC:需要的不是儲存的記憶體空間，而是遞迴需要的StackFrame空間，由遞迴深度決定
+    //Best Case:O(logn)
+    //Worst Case:O(n)
+    //Unstable Sorting
+    
+
+
+
+}
+
+
 
 int main(){
 
     vector<int>test={7,8,2,1,5,4,3,0};
-    InsertionSort(test);
-
+    
 
     return 0;
 }
