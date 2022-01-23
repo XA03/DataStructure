580c8435f23a7ca35251d7900962430a51d3a4d0
Chu <s033390431@gmail.com>
2022-01-23 19:41:05 +0800

    MergeSort Testing Comment

--- Sorting Algorithm/MergeSort.cpp
+++ Sorting Algorithm/MergeSort.cpp
@@ -10,6 +10,7 @@ void showlist(vector<int>arr){
 void Merge(vector<int>&arr,int left,int mid,int right){
     vector<int>leftsub(arr.begin()+left,arr.begin()+mid+1),
                rightsub(arr.begin()+mid+1,arr.begin()+right+1);
+    //把最大值放在後面如果其中一方到底了，就會自動排入剩下的一方的元素
     leftsub.push_back(INT_MAX);
     rightsub.push_back(INT_MAX);
     
@@ -24,13 +25,18 @@ void Merge(vector<int>&arr,int left,int mid,int right){
             arr[i]=rightsub[rightindex];
             rightindex++;
         }
-        
     }
+    showlist(arr);
 }
 
 void MergeSort(vector<int>&arr,int left,int right){
-    //流程說明：把陣列分成兩半，重複這個行為直到剩一個元素(分不了)然後在根據
-    
+    //流程說明：把陣列分成兩半，重複這個行為直到子陣列剩一個元素再開始將子陣列依照元素大小將其倆倆合併 最後得出排序陣列
+    //假設子陣列長度：A=m,B=n 合併兩個陣列的Merge  最長時間複雜度為m+n-1 最短為n 其中將m設為n的話 最長為2n-1 得O(n)
+    //接著計算有幾次合併，n次長度每次對半分則有logn次Recursive，所以得出 O(nlogn)
+    //Best Case: O(nlogn)
+    //Average Case: O(nlogn)
+    //Worst Case: O(nlogn)
+    //SC: O(n) 
     
     if(left<right){
         int mid=left+(right-left)/2;
@@ -40,17 +46,10 @@ void MergeSort(vector<int>&arr,int left,int right){
     }
 }
 
-
-
 int main(){
 
-    vector<int>test={7,8,2,1,5,3,4,0};
+    vector<int>test={8,7,2,1,5,3,4,0};
     MergeSort(test,0,test.size()-1);
-    showlist(test);
-    
-    
-
-
 
     return 0;
 }
